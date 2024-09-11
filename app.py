c0dcd6d2b9833f62402b554a3381089924653349
Катя Репьюк <repukekaterina@gmail.com>
2024-09-11 23:09:24 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/web")
 def start():
     return """<!doctype html>
-        "<html>"
-               <body>"
-                     <h1>web-сервер на flask</h1>"
-               </body>"
+        <html>
+            <body>
+                <h1>Web-сервер на flask</h1>
+            <a href="/autor">autor</a>
+            </body>
         </html>"""
 
+@app.route("/autor")
+def author():
+    name = "Репьюк Екатерина Дмитриевна"
+    group = "ФБИ-22"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """</p>
+                <p>Группа: """ + group + """</p>
+                <p>Группа: """ + faculty + """</p>
+                <a href="/web">web</a>
+            </body>
+        </html>"""
