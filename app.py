d2a8a3fd16d60aa948bad470684140fb45ad9589
Катя Репьюк <repukekaterina@gmail.com>
2024-09-18 22:37:41 +0700

    Стили

--- app.py
+++ app.py
@@ -33,12 +33,16 @@ def author():
 @app.route("/lab1/oak")
 def oak():
     path = url_for("static", filename="oak.jpg")
-    return '''
+    css_path = url_for("static", filename="lab1.css")
+    return f'''
     <!doctype html>
         <html>
+            <head>
+                <link rel="stylesheet" type="text/css" href="{css_path}">
+            </head>
             <body>
                 <h1>Дуб</h1>
-                <img src="''' + path + '''">
+                <img src="{path}">
             </body>
         </html>'''
 
