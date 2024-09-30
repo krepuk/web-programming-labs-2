7fbe9b64bde5211be97113e39a7311a0e739e71e
Катя Репьюк <repukekaterina@gmail.com>
2024-09-30 15:25:51 +0700

    Заголовок шаблона

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, redirect
+from flask import Flask, url_for, redirect, render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -335,3 +335,6 @@ def add_flower(name):
     </body>
 </html>
 '''
+@app.route('/lab2/example')
+def example():
+    return render_template('example.html')
