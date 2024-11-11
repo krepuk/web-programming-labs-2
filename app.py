059f639d924ec888f39f1807734cdf0e48d75c90
Катя Репьюк <repukekaterina@gmail.com>
2024-11-11 20:07:35 +0700

    Заготовка к 5 лабораторной

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
 
 app = Flask(__name__)
 
@@ -12,6 +13,7 @@ app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
 
 
 @app.route('/')
@@ -35,6 +37,9 @@ def web():
             </ul>
             <ul>
                 <li><a href="/lab4">Четвертая лабораторная</a></li>
+            </ul>
+            <ul>
+                <li><a href="/lab5">Пятая лабораторная</a></li>
             </ul>
                 <h1>Web-сервер на flask</h1>
             <a href="/lab1/autor">autor</a>
