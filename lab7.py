2fc441d9373f5a50d844f0a979c4ef2a81b6f999
Катя Репьюк <repukekaterina@gmail.com>
2024-12-08 00:27:47 +0700

    Вывод таблицы

--- lab7.py
+++ lab7.py
@@ -5,7 +5,7 @@ lab7 = Blueprint('lab7', __name__)
 
 @lab7.route('/lab7')
 def main():
-    return render_template('lab7/lab7.html')
+    return render_template('lab7/index.html')
 
 
 films = [
---
+++ static/lab7/main.js
@@ -0,0 +1,40 @@
+function fillfilmlist() {
+    fetch('/lab7/rest-api/films/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (films){
+        let tbody = document.getElementById('film-list');
+        tbody.innerHTML = '';
+        for(let i = 0; i < films.length; i++) {
+            let tr = document.createElement('tr');
+
+            let tdTitle = document.createElement('td');
+            let tdTitleRus = document.createElement('td');
+            let tdYear = document.createElement('td');
+            let tdActions = document.createElement('td');
+
+            tdTitle.innerText = films[i].title == films[i].title_ru ? '' : films[i].title;
+            tdTitleRus.innerText = films[i].title_ru;
+            tdYear.innerText = films[i].year;
+
+            let editButton = document.createElement('button');
+            editButton.innerText = 'редактировать';
+            editButton.onclick = function() { editfilm(i); };
+
+            let delButton = document.createElement('button');
+            delButton.innerText = 'удалить';
+            delButton.onclick = function() { deletefilm(i); };
+
+            tdActions.append(editButton);
+            tdActions.append(delButton);
+
+            tr.append(tdTitle);
+            tr.append(tdTitleRus);
+            tr.append(tdYear);
+            tr.append(tdActions);
+
+            tbody.append(tr);
+        }
+    });
+}
