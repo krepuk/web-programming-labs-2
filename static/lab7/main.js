044ec40fcc4ff606ca19615e50366998f710ca41
Катя Репьюк <repukekaterina@gmail.com>
2024-12-11 01:15:42 +0700

    добавление фильма

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -38,16 +38,59 @@ function fillfilmlist() {
             tr.append(tdActions);
 
             tbody.append(tr);
-
-            function deletefilm(id, title) {
-                if(! confirm(`Вы точно хотите удалить фильм"${title}"?`))
-                    return;
-
-                fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
-                    .then(function (){
-                        fillfilmlist();
-                    });
-            }
         }
+    });
+}
+
+function deletefilm(id, title) {
+    if(! confirm(`Вы точно хотите удалить фильм "${title}"?`))
+        return;
+
+    fetch(`/lab7/rest-api/films/${id}`, {method: 'DELETE'})
+        .then(function (){
+            fillfilmlist();
+        });
+}
+
+function showmodal(){
+    document.querySelector('.modal').style.display = 'block';
+}
+
+function hidemodal(){
+    document.querySelector('.modal').style.display = 'none';
+}
+
+function cancel() {
+    hidemodal();
+}
+
+function addfilm() {
+    document.getElementById('id').value = '';
+    document.getElementById('title').value = '';
+    document.getElementById('title-ru').value = '';
+    document.getElementById('year').value = '';
+    document.getElementById('description').value = '';
+    showmodal();
+}
+
+function sendfilm() {
+    const film = {
+        title: document.getElementById('title').value,
+        title_ru: document.getElementById('title-ru').value,
+        year: document.getElementById('year').value,
+        description: document.getElementById('description').value,
+    }
+    
+    const url = `/lab7/rest-api/films`;
+    const method = 'POST';
+
+    fetch(url, {
+        method: method,
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(film)
+    })
+    .then(function() {
+        fillfilmlist();
+        hidemodal();
     });
 }
